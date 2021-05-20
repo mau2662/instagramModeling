8aa3c446d8d57367c42a017294ffb1031fd1e7f9
mau2662 <vargas7384@gmail.com>
2021-05-20 01:31:27 +0000

    listo

--- src/models.py
+++ src/models.py
@@ -2,14 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Login(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+   
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
@@ -17,3 +17,58 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class UserPhotos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    photo = db.Column(db.String(120), unique=False, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=False)
+  
+
+    def __repr__(self):
+        return '<User %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "photo": self.photo,
+            "description": self.description,
+            # do not serialize the password, its a security breach
+        }
+
+class UserVideos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    video = db.Column(db.String(120), unique=False, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=False)
+  
+
+    def __repr__(self):
+        return '<User %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "video": self.video,
+            "description": self.description,
+            # do not serialize the password, its a security breach
+        }        
+
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(120), unique=False, nullable=False)
+    followers = db.Column(db.Integer, unique=False, nullable=False)
+    following = db.Column(db.Integer, unique=False, nullable=False)
+    photos= Column(String(250),ForeignKey('UserPhotos.photo'))
+    videos= Column(String(250),ForeignKey('UserVideos.video'))
+
+    def __repr__(self):
+        return '<User %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user,
+            "followers": self.followers,
+            "following": self.following,
+            
+            # do not serialize the password, its a security breach
+        }        
